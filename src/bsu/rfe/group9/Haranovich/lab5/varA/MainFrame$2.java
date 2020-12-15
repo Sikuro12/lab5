b7e89645c82e2050e8ea472dd1bc038a7db27c50
Haranovich Valiantsin <Sikuro12@yandex.by>
2020-12-15 23:31:46 +0300

    3commit

---
+++ src/bsu/rfe/group9/Haranovich/lab5/varA/MainFrame$2.java
@@ -0,0 +1,11 @@
+package bsu.rfe.group9.Haranovich.lab5.varA;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+class MainFrame$2 extends AbstractAction {
+    @Override
+    public void actionPerformed(final ActionEvent event) {
+        MainFrame.access$2(MainFrame.this).reset();
+    }
+}
